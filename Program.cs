708aaafadc60d6eeac3b5ac4fc63d3f036f8f984
lroeN <uzelefe684@gmail.com>
2025-04-22 22:06:00 +0300

    CollectingMachineApp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bu bir toplama makinesidir. Sayıları toplar.");
+Console.WriteLine("Sayıları girmeye başlayın. İşlemi bitirmek için 't' tuşuna basınız.");
+
+double toplam = 0;
+string input;
+
+while ((input = Console.ReadLine()) != "t")
+{
+    if (double.TryParse(input, out double sayi)) toplam += sayi;
+    else Console.WriteLine("Geçersiz giriş.");
+}
+
+Console.WriteLine($"Toplam: {toplam}");
